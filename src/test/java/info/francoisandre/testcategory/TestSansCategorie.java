71d6a43e8605f94ad0f3109725e5e17c7990a6a3
fandre5 <fandre5@P11773.academie.in.ac-toulouse.fr>
2018-02-27 12:03:28 +0100

    Ajout tests sans categorie

---
+++ src/test/java/info/francoisandre/testcategory/TestSansCategorie.java
@@ -0,0 +1,17 @@
+package info.francoisandre.testcategory;
+
+import org.junit.Test;
+
+public class TestSansCategorie {
+
+	@Test
+	public void test1() {
+		System.out.println("Ceci est le test sans categorie 1");
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("Ceci est le test sans categorie 2");
+	}
+
+}
